1bbf46e31d944a9786ca7f3d048e55658f71566e
Taofeek Abiodun Olasunkanmi Yusuf <dfntekniks@gmail.com>
2022-09-13 00:42:26 +0200

    main.tf edited

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "${var.resource_group}"
+    resource_group_name  = "Azuredevops"
     storage_account_name = "azuredevops19438"
     container_name       = "azuredevops-container"
     key                  = "prod.terraform.azuredevops-container"
