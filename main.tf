de237555b0d4c0f6b07712a4524c8e80f6442780
Taofeek Abiodun Olasunkanmi Yusuf <dfntekniks@gmail.com>
2022-09-12 22:52:22 +0200

    file modified

--- main.tf
+++ main.tf
@@ -23,9 +23,9 @@ locals {
 }
 
 module "resource_group" {
-  source         = "./modules/resource_group"
+  source          = "./modules/resource_group"
   resource_group  = var.resource_group
-  location       = var.location
+  location        = var.location
 }
 
 # Reference the AppService Module here.
