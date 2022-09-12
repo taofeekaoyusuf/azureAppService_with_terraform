a3be55e0a531853e2747901237ed634a7d2cb446
Taofeek Abiodun Olasunkanmi Yusuf <dfntekniks@gmail.com>
2022-09-12 17:54:53 +0200

    modify script file

--- storage_creation.sh
+++ storage_creation.sh
@@ -12,7 +12,7 @@ sleep 2
 # Variable Definition
 RESOURCE_GROUP_NAME="Azuredevops"
 STORAGE_ACCOUNT_NAME="azuredevops$RANDOM" #"Azuredevops$(get-random)"
-CONTAINER_NAME="azuredevops_container"
+CONTAINER_NAME="azuredevopscontainer"
 echo "\n***### Variable definition and Assignment Completed!!! ###***\n"
 sleep 5
 echo "\n"
