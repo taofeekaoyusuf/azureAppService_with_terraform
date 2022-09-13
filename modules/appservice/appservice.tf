8717b7e240269978bf1a7e56ceaf62cfec5a690d
Taofeek Abiodun Olasunkanmi Yusuf <dfntekniks@gmail.com>
2022-09-13 03:20:27 +0200

    appservice.tf modify

--- modules/appservice/appservice.tf
+++ modules/appservice/appservice.tf
@@ -1,7 +1,7 @@
 resource "azurerm_app_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
-  resource_group_name = azurerm_resource_group.dev.name
+  resource_group_name = var.resource_group
 
   sku {
     tier = "Free"
