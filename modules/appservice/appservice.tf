a8509f0ee30edd57a7d00288221d7b3bf995fc99
Taofeek Abiodun Olasunkanmi Yusuf <dfntekniks@gmail.com>
2022-09-13 02:03:52 +0200

    Update appservice.tf

--- modules/appservice/appservice.tf
+++ modules/appservice/appservice.tf
@@ -1,8 +1,8 @@
-resource "azurerm_app_service" "dev" {
+resource "azurerm_linux_web_app" "dev" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  app_service_plan_id = azurerm_app_service_plan.dev.id
+  app_service_plan_id = azurerm_linux_web_app_plan.dev.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 0
@@ -10,10 +10,10 @@ resource "azurerm_app_service" "dev" {
   tags = "${var.tags}"
 }
 
-resource "azurerm_app_service_plan" "dev" {
+resource "azurerm_linux_web_app_plan" "dev" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+  resource_group_name = azurerm_linux_web_app.dev.resource_group_name
 
   sku {
     tier = "Free"
