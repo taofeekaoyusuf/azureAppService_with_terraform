93078cd78ff5ad08838533ca316c38258d0bcc03
Taofeek Abiodun Olasunkanmi Yusuf <dfntekniks@gmail.com>
2022-09-13 15:59:56 +0200

    modified: appservice.tf

--- modules/appservice/appservice.tf
+++ modules/appservice/appservice.tf
@@ -1,7 +1,7 @@
 resource "azurerm_app_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+  resource_group      = "${var.resource_group}"
 
   sku {
     tier = "Free"
@@ -12,7 +12,7 @@ resource "azurerm_app_service_plan" "test" {
 resource "azurerm_app_service" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
+  resource_group      = "${var.resource_group}"
   app_service_plan_id = "${azurerm_app_service_plan.test.id}"
 
   app_settings = {
