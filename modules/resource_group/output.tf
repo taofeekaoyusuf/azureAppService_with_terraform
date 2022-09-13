cf5d13b84c01229b214aeb318c57d7d90c437a1b
Taofeek Abiodun Olasunkanmi Yusuf <dfntekniks@gmail.com>
2022-09-13 19:00:23 +0200

    Edited: files

--- modules/resource_group/output.tf
+++ modules/resource_group/output.tf
@@ -2,9 +2,9 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "resource_group" {
-  value = azurerm_resource_group.rg.name
-}
+# output "resource_group" {
+#   value = azurerm_resource_group.rg.name
+# }
 
 output "location" {
   value = azurerm_resource_group.rg.location
--- modules/resource_group/rg.tf
+++ modules/resource_group/rg.tf
@@ -1,7 +1,7 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group}"
-  location = "${var.location}"
-}
+# resource "azurerm_resource_group" "rg" {
+#   name     = "${var.resource_group}"
+#   location = "${var.location}"
+# }
 
 data "azurerm_resource_group" "rg"{
   name = azurerm_resource_group.rg.name
